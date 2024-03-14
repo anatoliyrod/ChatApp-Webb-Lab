118f758646115272a428b53cc159e2ecce8913b8
anatoliyrod <124780951+anatoliyrod@users.noreply.github.com>
2024-03-14 23:04:31 +0100

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatty.Hubs
+{
+    // Clasa pentru gestionarea conversațiilor în timp real
+    public class ChatHub : Hub
+    {
+        // Metoda pentru transmiterea mesajelor către toți clienții conectați
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}

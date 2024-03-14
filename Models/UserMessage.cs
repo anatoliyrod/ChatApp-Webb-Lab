4ffed733454c0575999aa9965945f8f564a62d4c
anatoliyrod <124780951+anatoliyrod@users.noreply.github.com>
2024-03-14 23:04:48 +0100

    Add files via upload

---
+++ Models/UserMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatty.Models
+{
+    // Clasa pentru mesajele utilizatorului
+    public class UserMessage
+    {
+        // Numele utilizatorului care a trimis mesajul
+        public string Username { get; set; }
+
+        // Conținutul mesajului
+        public string Message { get; set; }
+
+        // Indicator dacă mesajul este al utilizatorului curent sau nu
+        public bool CurrentUser { get; set; }
+
+        // Data și ora la care a fost trimis mesajul
+        public DateTime DateSent { get; set; }
+    }
+}
